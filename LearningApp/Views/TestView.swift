c8e6d3414029f2a195f49ac5c2057170d46168a0
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-11-10 20:21:04 -0500

    - Build scrollable answer options in TestView - Configure currently selected button to toggle gray - Configure possible answers to highlight green when correct and red if incorrect answer selected

--- LearningApp/Views/TestView.swift
+++ LearningApp/Views/TestView.swift
@@ -9,28 +9,80 @@ import SwiftUI
 
 struct TestView: View {
     @EnvironmentObject var contentModel: ContentModel
+    @State var selectedAnswerIndex: Int?
+    @State var numCorrect = 0
+    @State var submitted = false
     
     var body: some View {
         if let question = contentModel.currentQuestion {
-            VStack {
+            VStack (alignment: .leading) {
                 // Question number
-                // - known that cu
                 Text("Question \(question.id+1) of \(contentModel.currentModule?.test.questions.count ?? 0)")
+                    .padding(.leading, 20)
                 
                 // Question
                 CodeTextView()
+                    .padding(.horizontal, 20)
                 
                 // Answers
+                ScrollView {
+                    VStack {
+                        ForEach(0 ..< question.answers.count, id: \.self) { index in
+                            
+                            let cardColor = determineCardColor(index, correctIndex: question.correctIndex)
+                            
+                            ButtonView(
+                                theAction: {
+                                    selectedAnswerIndex = index
+                                },
+                                buttonText: question.answers[index],
+                                cardColor: cardColor,
+                                textColor: Color.black
+                            )
+                                .padding(.vertical, 5)
+                                .disabled(submitted)
+                        }
+                    }
+                    .padding()
+                }
                 
-                // Button
+                // Submit Button
+                ButtonView(theAction: {
+                    submitted = true
+                    
+                    // Check answer and keep track
+                    if selectedAnswerIndex == question.correctIndex {
+                        numCorrect += 1
+                    }
+                }, buttonText: "Submit")
+                    .padding()
+                    .disabled(selectedAnswerIndex == nil)
             }
-            .padding()
             .navigationBarTitle("\(contentModel.currentModule?.category ?? "") Test")
             .accentColor(.black)
         } else {
             // Gives moment for currentQuestion to be set
             ProgressView()
         }
+    }
+    
+    private func determineCardColor(_ index: Int, correctIndex: Int) -> Color {
+        var result = Color.white
+        
+        if submitted {
+            // Correct answer should always be green - no matter if selected or not
+            if index == correctIndex {
+                result = Color.green
+            } else if index == selectedAnswerIndex {
+                // If reached, this choice isn't correct. If it was the selected choice,
+                // it needs to be red
+                result = Color.red
+            }
+        } else if selectedAnswerIndex == index {
+            result = Color.gray
+        }
+        
+        return result
     }
 }
 
