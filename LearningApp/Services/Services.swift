d9c0c3705045d2ebb1a82746187e972043590838
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-11-13 14:52:10 -0500

    Factor local json decoding out of Services

--- LearningApp/Services/Services.swift
+++ LearningApp/Services/Services.swift
@@ -6,35 +6,9 @@
 //
 
 import Foundation
+import SwiftUI
 
 class Services {
-    
-    // Using generics for possible reuse later in the app
-    static func decodeLocalJson<T: Decodable>(filename: String) -> [T] {
-        var result = [T]()
-        
-        // Get URL
-        if let url = Bundle.main.url(forResource: filename, withExtension: "json") {
-            // Get data at url
-            do {
-                let data = try Data(contentsOf: url)
-                
-                // Decode the data
-                do {
-                    let jsonDecoder = JSONDecoder()
-                    result = try jsonDecoder.decode([T].self, from: data)
-                } catch {
-                    // Could not decode the data
-                    print(error)
-                }
-            } catch {
-                // Could not get data object from url
-                print(error)
-            }
-        }
-        
-        return result
-    }
     
     static func parseStyleHtml(filename: String) -> Data? {
         var result: Data?
