ab28d6ebb9dd834e0d3e4ecf5649466c8fc4a4b1
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-11-13 14:50:40 -0500

    Provide slight bottom padding after each content + test group

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -41,6 +41,7 @@ struct HomeView: View {
                                     }
                             }
                         }
+                        .padding(.bottom, 15)
                     }
                     .accentColor(.black)
                     .padding()
